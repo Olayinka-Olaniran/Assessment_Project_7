416027a22738e2b703787a456984fc449b296654
Olayinka <oolaniran853@gmail.com>
2026-07-10 10:50:39 +0100

    exported quizQuestions to the main js module and added a frame work of functions

---
+++ WeeklyAssessment7.js
@@ -0,0 +1,39 @@
+import { QuizQuestions } from './quizQuestions.js';
+
+console.log(QuizQuestions.css[0].question);
+
+function renderQuizQuestions(){
+	
+}
+
+function storeChosenAnswers(){
+
+}
+
+function getChosenAnswers(){
+	
+}
+
+function prevQuestion(){
+
+}
+
+function nextQuestion(){
+
+}
+
+function moveToQuestion(){
+
+}
+
+function renderQuizResults(){
+
+}
+
+function renderQuizReview(){
+
+}
+
+function finishQuiz(){
+
+}
